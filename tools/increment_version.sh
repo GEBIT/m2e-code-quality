17a25fdcb932e562d28c1938f22f74f059dc5b56
Erwin Tratar <erwin.tratar@gebit.de>
2018-11-08 14:15:53 +0100

    Increment version fixed

--- tools/increment_version.sh
+++ tools/increment_version.sh
@@ -2,24 +2,37 @@
 
 [ "$#" -ne "1" ] && echo "usage: $0 <current version>" && exit 1
 
-SITE_GITHUB_BRANCH=develop
+GITHUB_BRANCH=deploy
 
 DIR=$(cd `dirname $0` && echo `git rev-parse --show-toplevel`)
-NEW_VERSION=$(echo $1 | awk 'BEGIN { FS=":" } { $3++;  if ($3 > 99) { $3=0; $2++; if ($2 > 99) { $2=0; $1++ } } } { printf "%02d:%02d:%02d\n", $1, $2, $3 }')-SNAPSHOT
+NEW_VERSION=$(echo $1 | awk 'BEGIN { FS=":" } { $3++; } { printf "%d.%d.%d\n", $1, $2, $3 }')-SNAPSHOT
 
 cd $DIR
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
+# make sure no stale stuff before checkout
 git reset --hard
-git checkout develop
 
+# setup origin
+git remote set-url origin https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+git fetch 
+
+# verify same commit as branch
+A=$(git rev-parse --verify HEAD)
+B=$(git rev-parse --verify origin/$GITHUB_BRANCH)
+
+echo "Comparing $A to $B on $GITHUB_BRANCH"
+if [ "$A" -ne "$B" ]; then exit 0; fi
+
+# checkout branch
+git checkout $GITHUB_BRANCH
+
+# increment version
+echo changing version to $NEW_VERSION
 mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$NEW_VERSION -Dtycho.mode=maven
 
-git commit -a --message "Next version: $NEW_VERSION"
-
-git remote set-url origin  https://travis:${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git 
-#> /dev/null 2>&1
-git push origin
-#--quiet --set-upstream origin-pages gh-pages 
+# push changes to branch
+git commit -a --message "Next version: $NEW_VERSION" > /dev/null 2>&1
+git push origin --quiet 
